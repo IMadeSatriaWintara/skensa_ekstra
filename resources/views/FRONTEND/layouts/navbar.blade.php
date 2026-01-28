062b45e67bd3f38561ed0d2f463daa8b5dadc738
respa1 <respamks@gmail.com>
2026-01-28 22:21:58 +0800

    frontend dikit

---
+++ resources/views/FRONTEND/layouts/navbar.blade.php
@@ -0,0 +1,21 @@
+<!-- Header Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" height="40">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#berondo">Beranda</a></li>
+                <li class="nav-item"><a class="nav-link" href="#eskul">Eskul</a></li>
+                <li class="nav-item"><a class="nav-link" href="#berita">Berita</a></li>
+                <li class="nav-item"><a class="nav-link" href="#prestasi">Prestasi</a></li>
+                <li class="nav-item"><a class="nav-link" href="#tentang">Tentang</a></li>
+                <li class="nav-item"><a class="nav-link btn btn-primary text-white ms-2" href="#daftar">Daftar</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
